7f596d7579e66dfbf5624ec4c67be952eb9751dc
Sudimport <admin@sudimport.website>
2025-05-01 21:40:37 +0000

    fix: spostato script in Custom Script per evitare build

--- purchase_invoice_supplier_linker/hooks.py
+++ purchase_invoice_supplier_linker/hooks.py
@@ -4,3 +4,10 @@ app_publisher = "Thomas"
 app_description = "Script per collegare articoli a codici fornitore automaticamente"
 app_email = "thomas@example.com"
 app_license = "MIT"
+
+fixtures = [
+    {
+        "doctype": "Custom Script",
+        "filters": [["name", "=", "Purchase Invoice Item-Client"]]
+    }
+]
